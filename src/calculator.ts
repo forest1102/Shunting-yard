31dc323c92ee37168b3b7ca85b27888d5860ba2f
issei morita <isseimorita1@gmail.com>
2018-10-15 00:38:47 -0700

    enable to use funcs

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,5 +1,6 @@
 import * as isNumber from 'is-number'
 type Token = { precedence?: number, LeftAssociative?: Boolean, func: (a: number, b: number) => number }
+type Func = { func: (a: number, b?: number) => number, binary: boolean }
 export default class Calculator {
 	private readonly operatorsStack: string[] = []
 	private readonly output: string[] = []
@@ -22,11 +23,15 @@ export default class Calculator {
 				this.postfix.push(token)
 				this.output.push(token)
 			}
+			else if (this.isFunc(token)) {
+				this.operatorsStack.push(token)
+			}
 			else if (this.isOperator(token)) {
 
 				let top = this.operatorsStack[this.operatorsStack.length - 1]
 
-				while (top !== undefined && Calculator.operators[top] !== undefined && (
+				while (top !== undefined && top !== '(' && (
+					this.isFunc(token) ||
 					(Calculator.operators[token].LeftAssociative &&
 						(Calculator.operators[token].precedence <= Calculator.operators[top].precedence)) ||
 					(!Calculator.operators[token].LeftAssociative &&
@@ -46,6 +51,9 @@ export default class Calculator {
 				}
 				this.operatorsStack.pop()
 			}
+			else {
+				throw new Error('unexpected character.\nMaybe you forgot the space or mistype func name')
+			}
 		}
 
 	}
@@ -53,9 +61,22 @@ export default class Calculator {
 	calc() {
 		console.log(this.operatorsStack, this.output, this.postfix)
 		const op = this.operatorsStack.pop()
-		const b = this.output.pop()
-		const a = (this.result === undefined) ? this.output.pop() : this.result
-		this.result = Calculator.operators[op].func(+a, +b)
+		if (this.isOperator(op)) {
+			const b = this.output.pop()
+			const a = (this.result === undefined) ? this.output.pop() : this.result
+			this.result = Calculator.operators[op].func(+a, +b)
+		}
+		else if (this.isFunc(op)) {
+			if (Calculator.funcs[op].binary) {
+				const b = this.output.pop()
+				const a = (this.result === undefined) ? this.output.pop() : this.result
+				this.result = Calculator.funcs[op].func(+a, +b)
+			}
+			else {
+				const a = (this.result === undefined) ? this.output.pop() : this.result
+				this.result = Calculator.funcs[op].func(+a)
+			}
+		}
 		console.log(this.result)
 		this.postfix.push(op)
 		return this.operatorsStack[this.operatorsStack.length - 1]
@@ -64,6 +85,32 @@ export default class Calculator {
 	get getResult() {
 		return this.result
 	}
+
+	static readonly funcs: { [key: string]: Func } = Object.freeze(
+		{
+			'sqrt': {
+				func: a => Math.sqrt(a),
+				binary: false
+			},
+			'log': {
+				func: (a, b) => Math.log(b) / Math.log(a),
+				binary: true
+			},
+			'ln': {
+				func: a => Math.log(a),
+				binary: false
+			},
+			'sin': {
+				func: (a) => Math.sin(a),
+				binary: false
+			},
+			'nthrt': {
+				func: (a, b) => Math.pow(a, 1 / b),
+				binary: true
+			}
+		}
+	)
+
 
 	static readonly operators: { [key: string]: Token } = Object.freeze(
 		{
@@ -96,7 +143,11 @@ export default class Calculator {
 	)
 
 	isOperator(token: string) {
-		return Calculator.operators[token] !== undefined
+		return Calculator.operators.hasOwnProperty(token)
+	}
+
+	isFunc(token: string) {
+		return Calculator.funcs.hasOwnProperty(token)
 	}
 
 	get getPostfix() {
