8c089862e934be253e3a306f5f13474bb70dd9ad
issei morita <isseimorita1@gmail.com>
2018-10-15 17:31:27 -0700

    make command line calculator

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,26 @@
-import * as isNumber from 'is-number'
 import Calculator from './calculator'
-const calc = new Calculator('log10 ( 10 * 10 ^ 2 )')
-console.log('result:', calc.getResult)
-console.log('postfix:', calc.getPostfix)
+import * as readline from 'readline'
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+})
+
+const askCalculation = () => {
+	rl.question('Calculate:\n', tokens => {
+		if (tokens === 'exit' || tokens === '') {
+			rl.close()
+			return
+		}
+		const calc = new Calculator(tokens)
+
+		console.log('result:', calc.getResult)
+		console.log('postfix:', calc.getPostfix)
+		askCalculation()
+	})
+}
+askCalculation()
+
+
+
 
